201eff4ef631fa959a87318adb0a227e4846883b
PortableDoughnut <gwen.thelin@gmail.com>
2024-09-26 09:25:06 -0600

    inital commit

---
+++ My FamilyTests/My_FamilyTests.swift
@@ -0,0 +1,17 @@
+//
+//  My_FamilyTests.swift
+//  My FamilyTests
+//
+//  Created by Gwen Thelin on 9/26/24.
+//
+
+import Testing
+@testable import My_Family
+
+struct My_FamilyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
