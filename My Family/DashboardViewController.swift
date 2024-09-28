121d36e843e14184170492a6fb9f50a1cdc91a09
PortableDoughnut <gwen.thelin@gmail.com>
2024-09-28 14:37:38 -0600

    Got the tapping working.

--- My Family/DashboardViewController.swift
+++ My Family/DashboardViewController.swift
@@ -14,6 +14,24 @@ class DashboardViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func coffeeGwenTapped(_ sender: UITapGestureRecognizer) {
+        print("Coffee Gwen")
+    }
+    
+    @IBAction func footballMeaganTapped(_ sender: UITapGestureRecognizer) {
+        print("Football Meagan")
+    }
+    
+    @IBAction func madsHunnyTapped(_ sender: UITapGestureRecognizer) {
+        print("Mads Hunny")
+    }
+    
+    @IBAction func brettCameraTapped(_ sender: UITapGestureRecognizer) {
+        print("Brett Camera")
+    }
+    
+    @IBAction func AliciaDonkeyTapped(_ sender: UITapGestureRecognizer) {
+        print("Alicia Donkey")
+    }
 }
 
